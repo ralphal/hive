38a237259322560996daed3b51f991c6ee7ff26b
wujunzheng <wujunzheng@yy.com>
2018-12-27 19:25:29 +0800

    firstblood

--- hiveclient.go
+++ hiveclient.go
@@ -1,80 +1,56 @@
-/*
-Hive:  The go thrift library for connecting to hive server.
-
-This is just the generated Thrift-Hive and a very small connection wrapper.
-
-
-Usage:
-
-    func main() {
-
-      hive.MakePool("192.168.1.17:10000")
-
-      conn, err := GetHiveConn()
-      if err == nil {
-        er, err := conn.Client.Execute("SELECT * FROM logevent")
-        if er == nil && err == nil {
-          for {
-            row, _, _ := conn.Client.FetchOne()
-            log.Println("row ", row)
-          }
-        }
-      }
-      if conn != nil {
-        // make sure to check connection back into pool
-        conn.Checkin()
-      }
-    }
-
-*/
 package hive
 
 import (
+	"errors"
 	"log"
+	"time"
 
 	"git.apache.org/thrift.git/lib/go/thrift"
 	thrifthive "github.com/ralphal/hive/thriftlib"
 )
 
-type HiveConnection struct {
-	Server        string
-	Id            int
-	Client        *thrifthive.TCLIServiceClient
-	SessionHandle *thrifthive.TSessionHandle
+// Connection ...
+type Connection struct {
+	Server string
+	Client *thrifthive.TCLIServiceClient
+
+	SessionHandle   *thrifthive.TSessionHandle
+	OperationHandle *thrifthive.TOperationHandle
 }
 
-var hivePool chan *HiveConnection
+var (
+	pool  chan *Connection
+	_user     string
+	_password string
 
-// create connection pool, initialize connections
+	_batchsize int64 = 1000
+	_sleep = 1
+)
+
+// SetUserPassword ...
+func SetUserPassword(user, password string) {
+	_user = user
+	_password = password
+}
+
+//MakePool create connection pool, initialize connections
 func MakePool(server string) {
 
-	hivePool = make(chan *HiveConnection, 100)
+	pool = make(chan *Connection, 100)
 
 	for i := 0; i < 100; i++ {
-		// add empty values to the pool
-		hivePool <- &HiveConnection{Server: server, Id: i}
+		pool <- &Connection{Server: server}
 	}
 
 }
 
-// main entry point for checking out a connection from a list
-func GetHiveConn() (conn *HiveConnection, err error) {
-	//configMu.Lock()
-	//keyspaceConfig, ok := configMap[keyspace]
-	//if !ok {
-	//  configMu.Unlock()
-	//  return nil, errors.New("Must define keyspaces before you can get connection")
-	//}
-	//configMu.Unlock()
-
+//GetHiveConn main entry point for checking out a connection from a list
+func GetHiveConn() (conn *Connection, err error) {
 	return getConnFromPool()
 }
 
-func getConnFromPool() (conn *HiveConnection, err error) {
-
-	conn = <-hivePool
-	log.Printf("in checkout, pulled off pool: remaining = %d, connid=%d Server=%s\n", len(hivePool), conn.Id, conn.Server)
-	// BUG(ar):  an error occured on batch mutate <nil> <nil> <nil> Cannot read. Remote side has closed. Tried to read 4 bytes, but only got 0 bytes.
+func getConnFromPool() (conn *Connection, err error) {
+	conn = <-pool
 	if conn.Client == nil || conn.Client.Transport.IsOpen() == false {
 
 		err = conn.Open()
@@ -84,8 +60,8 @@ func getConnFromPool() (conn *HiveConnection, err error) {
 	return
 }
 
-// opens a hive connection
-func (conn *HiveConnection) Open() error {
+//Open opens a hive connection
+func (conn *Connection) Open() error {
 
 	log.Println("creating new hive connection ")
 
@@ -98,6 +74,8 @@ func (conn *HiveConnection) Open() error {
 
 	sreq := thrifthive.NewTOpenSessionReq()
 	sreq.ClientProtocol = 6
+	sreq.Username = &_user
+	sreq.Password = &_password
 
 	r, err := conn.Client.OpenSession(sreq)
 	if err != nil {
@@ -111,22 +89,107 @@ func (conn *HiveConnection) Open() error {
 	return nil
 }
 
-func (conn *HiveConnection) Checkin() {
-
-	hivePool <- conn
+// Checkin ...
+func (conn *Connection) Checkin() {
+	pool <- conn
 }
 
+// Exec ...
+func (conn *Connection) Exec(query string, async bool) error {
+	req := thrifthive.NewTExecuteStatementReq()
+	req.SessionHandle = conn.SessionHandle
+	req.Statement = query
+	req.RunAsync = async
 
-func (conn *HiveConnection) Exec(cmd string) error {
-  req := thrifthive.NewTExecuteStatementReq()
-  req.SessionHandle = conn.SessionHandle
-  req.Statement = cmd
-  r, err := conn.Client.ExecuteStatement(req)
-  if err != nil {
-    log.Println("err=", r)
-    return err
-  }
+	resp, err := conn.Client.ExecuteStatement(req)
+	if err != nil {
+		return err
+	}
 
-  log.Println("r=", r)
-  return nil
+	if !isOK(resp.Status) {
+		return errors.New("error from server:" + resp.Status.String())
+	}
+
+	conn.OperationHandle = resp.OperationHandle
+
+	return nil
+}
+
+// GetMetaData ...
+func (conn *Connection) GetMetaData() (*thrifthive.TTableSchema, error) {
+	req := thrifthive.NewTGetResultSetMetadataReq()
+	req.OperationHandle = conn.OperationHandle
+
+	resp, err := conn.Client.GetResultSetMetadata(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	schema := resp.GetSchema()
+
+	return schema, nil
+}
+
+func isOK(p *thrifthive.TStatus) bool {
+	status := p.GetStatusCode()
+	return status == thrifthive.TStatusCode_SUCCESS_STATUS || status == thrifthive.TStatusCode_SUCCESS_WITH_INFO_STATUS
+}
+
+// FetchRows ...
+func (conn *Connection) FetchRows() (*thrifthive.TRowSet, bool, error) {
+	req := thrifthive.NewTFetchResultsReq()
+	req.OperationHandle = conn.OperationHandle
+	req.Orientation = thrifthive.TFetchOrientation_FETCH_NEXT
+	req.MaxRows = _batchsize
+
+	socket := conn.Client.Transport.(*thrift.TSocket)
+	socket.SetTimeout(10 * time.Second)
+
+	resp, err := conn.Client.FetchResults(req)
+	if err != nil {
+		return nil, false, err
+	}
+
+	if !isOK(resp.Status) {
+		return nil, false, errors.New("fetchResults failed, " + resp.Status.String())
+	}
+
+	return resp.Results, *resp.HasMoreRows, nil
+}
+
+// Wait ...
+func (conn *Connection) Wait() (bool, error) {
+	for {
+		status, state, err := conn.GetStatus()
+		if err != nil {
+			return false, err
+		}
+
+		if state == thrifthive.TOperationState_RUNNING_STATE || status.StatusCode == thrifthive.TStatusCode_STILL_EXECUTING_STATUS {
+			time.Sleep(time.Duration(_sleep) * time.Second)
+			continue
+		}
+
+		if !isOK(status) {
+			return false, nil
+		}
+
+		break
+	}
+
+	return true, nil
+}
+
+// GetStatus ...
+func (conn *Connection) GetStatus() (*thrifthive.TStatus, thrifthive.TOperationState, error) {
+	req := thrifthive.NewTGetOperationStatusReq()
+	req.OperationHandle = conn.OperationHandle
+
+	resp, err := conn.Client.GetOperationStatus(req)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return resp.Status, *resp.OperationState, nil
 }
